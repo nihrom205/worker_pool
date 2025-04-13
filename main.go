df019362f69d943e0c0aa0398dae29e2320e025c
Alexey Rastorguev <rastorguev00@gmail.com>
2025-04-13 16:38:38 +0500

    feat: add implemented worker pool

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Pool struct {
+	pool chan struct{}
+}
+
+func NewPool(numberWorker int) *Pool {
+	pool := make(chan struct{}, numberWorker)
+	for i := 0; i < numberWorker; i++ {
+		pool <- struct{}{}
+	}
+	return &Pool{pool: pool}
+}
+
+func (p *Pool) work(n int) {
+	<-p.pool
+	go func() {
+		time.Sleep(2 * time.Second)
+		fmt.Println(n, "pool start")
+		p.pool <- struct{}{}
+	}()
+
+}
+
+func main() {
+	pool := NewPool(3)
+	for i := 0; i < 10; i++ {
+		pool.work(i)
+	}
+	time.Sleep(20 * time.Second)
+}
